6c27ecf74c26b92c875aa9381563b107c1b8421d
Lilja Kiiski <lilja.maria.kiiski@gmail.com>
2023-03-13 16:33:27 -0700

    Basic idea working except model is trash

--- myapp/templates/run.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-export CUDA_VISIBLE_DEVICES=""
-
-python3 test.py
-
-#google-chrome index.html
--- myapp/views.py
+++ myapp/views.py
@@ -1,10 +1,16 @@
 from django.shortcuts import render
+from transformers import pipeline
+from pprint import pprint
+
+generator = pipeline('text-generation', model='distilgpt2')
 
 # Create your views here.
 def index(request):
-    a = 2+2
-    context = {"result": a}
-    return render(request, "index.html", context)
 
-def house(request):
-    return render(request, "index.html")
+    message = ""
+    if request.method == "POST":
+        prompt = request.POST.get("input", "")
+        message = generator(prompt, min_length=30, max_length=60)
+    context = {"message": message}
+
+    return render(request, "index.html", context)
