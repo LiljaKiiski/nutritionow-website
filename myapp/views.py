069bf57a6b4d0821ee6303ab92e0d34ff83e8b82
Lilja Kiiski <lilja.maria.kiiski@gmail.com>
2023-03-13 16:20:19 -0700

    django stuff

--- myapp/views.py
+++ myapp/views.py
@@ -2,4 +2,9 @@ from django.shortcuts import render
 
 # Create your views here.
 def index(request):
+    a = 2+2
+    context = {"result": a}
+    return render(request, "index.html", context)
+
+def house(request):
     return render(request, "index.html")
