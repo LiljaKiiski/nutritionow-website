0c9e316459812995490fe58ef5fd3978d1bb20de
Lilja Kiiski <lilja.maria.kiiski@gmail.com>
2023-03-14 15:42:23 -0700

    it works!

--- install.sh
+++ install.sh
@@ -2,17 +2,7 @@
 
 #Install all dependencies required
 
-#this link for conda
-#https://docs.conda.io/en/latest/miniconda.html?ref=learn-ubuntu#linux-installers
-
-pip install django, django-environ
-
-pip install nltk, numpy
-
-# pip install torch torchvision
-
-# pip install transformers
-
-# conda install -c huggingface transformers
-
-# conda install pytorch torchvision -c soumith
+pip install django
+pip install django-environ
+pip install nltk
+pip install numpy
