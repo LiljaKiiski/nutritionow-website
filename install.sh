35aeab188d255abeb5c4f94203439b8e78a68cf1
Jayden <sqwatato@gmail.com>
2023-03-15 15:07:26 -0700

    cool stuff

--- install.sh
+++ install.sh
@@ -4,8 +4,4 @@ pip install django
 pip install django-environ
 pip install nltk
 pip install numpy
-pip install mkl
-pip install tflearn
-pip install tensorflow
-pip install scipy
-pip install pyyaml h5py
+pip install torch
