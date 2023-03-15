6c929b7e3292c9e4b4b28b48683e6de016884c29
Jayden <sqwatato@gmail.com>
2023-03-15 13:29:24 -0700

    chatgpt reworded

---
+++ chatgpt.py
@@ -0,0 +1,41 @@
+import http.client
+import json
+
+conn = http.client.HTTPSConnection("chatgpt-api.shn.hk")
+headers = { 'Content-Type': "application/json" }
+with open("intents.json") as f:
+    end = json.load(f)
+    
+# end = {"intents": []}
+
+with open("FoodQandA.json", 'r') as f:
+    intents = json.load(f) 
+c = 1
+for intent in intents['intents']:
+    print(c)
+    if c < 35:
+        c += 1
+        continue
+    string = intent['patterns'][0]
+    message = "Rewrite the question " + string + " 10 times while separating each question with commas and no space"
+    payload = "{\"model\": \"gpt-3.5-turbo\",\"messages\": [{\"role\": \"user\", \"content\": \"" + message + "\"}]}"
+    conn.request("POST", "/v1/", payload, headers)
+    res = conn.getresponse()
+    data = json.loads(res.read())
+    ans = data['choices'][0]['message']['content'].replace("\n", "").split(",")
+    ans.append(string)
+    tmp = {}
+    tmp["tag"] = intent['tag']
+    # print(ans)
+    tmp["patterns"] = ans
+    # print(tmp["patterns"])
+    tmp["responses"] = [intent['responses']]
+    tmp["context_set"] = ""
+    end["intents"].append(tmp)
+    c += 1
+    obj = json.dumps(end)
+    with open("intents.json", "w") as f:
+        f.write(obj)
+
+
+# {'id': 'chatcmpl-6uQZcJbIXh89i6dgjxWZGmw4t0iJ7', 'object': 'chat.completion', 'created': 1678906536, 'model': 'gpt-3.5-turbo-0301', 'usage': {'prompt_tokens': 35, 'completion_tokens': 132, 'total_tokens': 167}, 'choices': [{'message': {'role': 'assistant', 'content': "\n\nWhat is the suggested daily caloric intake for a typical adult? \nWhat's the standard calorie recommendation for an average adult? \nWhat's the advised calorie intake for a regular adult? \nHow much calories are recommended on a daily basis for the average adult? \nFor an average adult, what's the daily advised calorie intake? \nWhat quantity of daily calories are recommended for an ordinary adult? \nWhat's the typical caloric recommendation for a regular adult? \nWhat's the everyday adult's advised daily calorie intake? \nWhat's the standard every day caloric intake for an average adult? \nWhat is a regular adult's suggested daily calorie intake?"}, 'finish_reason': 'stop', 'index': 0}]}
