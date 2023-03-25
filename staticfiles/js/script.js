15811e5c52b22d518e1a5e67a6907ba1790b0251
Jayden <sqwatato@gmail.com>
2023-03-24 20:47:55 -0700

    ui changes

--- staticfiles/js/script.js
+++ staticfiles/js/script.js
@@ -1,23 +1,28 @@
-document.querySelector("button.expand").addEventListener(
-	"click",
-	function (e) {
-		e.preventDefault();
-		e.stopPropagation();
-		const button = e.currentTarget;
-		button.classList.add("loading");
-		button.disabled = true;
-		setTimeout(() => {
-			button.classList.add("loaded");
-			setTimeout(() => {
-				button.classList.add("finished");
-				setTimeout(() => {
-					button.classList.remove("finished");
-					button.classList.remove("loaded");
-					button.classList.remove("loading");
-					button.disabled = false;
-				}, 1500);
-			}, 700);
-		}, 1500);
-	},
-	false
-);
+document.addEventListener("DOMContentLoaded", () => {
+    document.querySelector("button.expand").addEventListener(
+        "click",
+        function (e) {
+            // e.preventDefault();
+            const button = e.currentTarget;
+            const txtarea = document.querySelector("#input");
+            document.querySelector('#chat-form').submit();
+            txtarea.disabled = true;
+            button.classList.add("loading");
+            button.disabled = true;
+            setTimeout(() => {
+                button.classList.add("loaded");
+                setTimeout(() => {
+                    button.classList.add("finished");
+                    setTimeout(() => {
+                        button.classList.remove("finished");
+                        button.classList.remove("loaded");
+                        button.classList.remove("loading");
+                        button.disabled = false;
+                    }, 1500);
+                }, 700);
+            }, 1500);
+            txtarea.disabled = false;
+        }
+    );
+});
+
